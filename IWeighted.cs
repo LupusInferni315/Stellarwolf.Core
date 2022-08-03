1e20ac0a8cb5e41fdc90bf0faf1cdbbc8d7ac111
Raistlin Wolfe <jdoonan61@gmail.com>
2022-08-02 21:46:01 -0600

    Create IWeighted.cs

---
+++ IWeighted.cs
@@ -0,0 +1,17 @@
+namespace StellarWolf.Core
+{
+
+    /// <summary>
+    /// A base for weighted objects.
+    /// </summary>
+    public interface IWeighted
+    {
+    
+        /// <summary>
+        /// The weight of the object.
+        /// </summary>
+        int Weight { get; }
+        
+    }
+    
+}
