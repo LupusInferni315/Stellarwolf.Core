b6cca6762bf5c8c05d2fb2f11c09c02f63573e5c
Raistlin Wolfe <jdoonan61@gmail.com>
2022-08-02 21:45:19 -0600

    Create DieType.cs

---
+++ DieType.cs
@@ -0,0 +1,24 @@
+namespace StellarWolf.Core
+{
+
+    /// <summary>
+    /// Defines the standard set of dice
+    /// </summary>
+    public enum DieType
+    {
+        /// <summary>Represents a standard 4 sided die.</summary>
+        D4 = 4,
+        /// <summary>Represents a standard 6 sided die.</summary>
+        D6 = 6,
+        /// <summary>Represents a standard 8 sided die.</summary>
+        D8 = 8,
+        /// <summary>Represents a standard 10 sided die.</summary>
+        D10 = 10,
+        /// <summary>Represents a standard 12 sided die.</summary>
+        D12 = 12,
+        /// <summary>Represents a standard 20 sided die.</summary>
+        D20 = 20,
+        /// <summary>Represents a standard 10 sided die and a percentile die.</summary>
+        D100 = 100
+    }
+}
